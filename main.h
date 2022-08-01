c86bafef46284835c5ae610ba6bd629ffa566d2d
Sheila20 <shilanyambura47@gmail.com>
2022-08-01 11:57:08 -0700

    our shell project

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+int _putchar(int c);
+#endif
---
+++ simple.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - prints out a single character
+ * @c: the character to be printed out
+ * Return: success (1)
+ */
+int _putchar(int c)
+{
+write(1, &c, 1);
+return (1);
+}
