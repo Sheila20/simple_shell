084fe105498f71d318ec9a32d248910b666ba64d
Sheila20 <shilanyambura47@gmail.com>
2022-08-08 13:18:38 -0700

    our shell project

---
+++ _strcat.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: resulting string.
+ * @src: source string.
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, n;
+n = string_length(dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+for (i = 0; i < n && src[i] != '\0'; i++)
+}
+dest[n + i] = '\0';
+return (dest);
+}
